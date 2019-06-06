0ba2ae269b582487e1598a825b24e87d21e5cf6a
Yogesh1018 <yogesh.raghuvanshi@gemsessence.com>
2019-06-06 17:16:16 +0530

    create booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,18 @@
 class BookingsController < ApplicationController
 
+  before_action :find_ticket, only: [:new]
+
   def new
-    
+    @booking = Booking.new
   end
 
+  def create
+    redirect_to ticket_path  
+  end
+
+  private
+
+  def find_ticket
+    @ticket = MovieTicket.find(params[:ticket_id])
+  end
 end
